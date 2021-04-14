9a6a0eca8c9fd996e17bad24fb96b942c003e6fd
Aung <aungheinmynn@gmail.com>
2021-04-14 18:18:52 +0800

    image upload

--- server/routers/newsletter.js
+++ server/routers/newsletter.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const newsletterRoutes = express.Router();
 const jwt = require("jsonwebtoken");
+const cloudinary = require("cloudinary").v2;
+const multer = require("multer");
 
 let Newsletter = require("../models/newsletter.model");
 
@@ -100,5 +102,57 @@ newsletterRoutes.route("/update/:id").post((req, res) => {
       res.status(400).send("adding new Newsletter failed");
     });
 });
+
+let cloudinaryConfig = {
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+};
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public");
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+let upload = multer({ storage: storage }).single("file");
+
+const uploadRiderImage = async (req, res) => {
+  try {
+    let filePath = req.file;
+
+    const uploadImage = new Promise((resolve, reject) => {
+      cloudinary.config(cloudinaryConfig);
+      cloudinary.uploader
+        .upload(filePath.path, {
+          folder: "010221.org",
+          unique_filename: true,
+        })
+        .then((result) => {
+          let imageUrl = result.secure_url;
+          return resolve(imageUrl);
+        })
+        .catch((err) => err);
+    });
+
+    let image = await uploadImage;
+    return res.status(200).json({ success: true, data: image });
+  } catch (err) {
+    console.log("error", err);
+    return res.status(500).json({ success: true, data: {} });
+  }
+};
+const uploadStore = multer({
+  storage,
+});
+
+newsletterRoutes.post(
+  "/imageupload",
+  uploadStore.single("file"),
+  uploadRiderImage
+);
 
 module.exports = newsletterRoutes;
